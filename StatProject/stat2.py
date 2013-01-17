f2f2815225d5c792de2af14f9a278e897edc5405
Shauna GM <shaunagm@gmail.com>
2013-01-16 22:51:11 -0500

    Finished first draft of stat2.py.

--- StatProject/stat2.py
+++ StatProject/stat2.py
@@ -1,10 +1,27 @@
 import csv
+import numpy
+
+data = ["amount","recipient"]
 
 with open('contributions.csv', 'rb') as csvfile:
 	contributions = csv.reader(csvfile, delimiter=',')
 	for row in contributions:
+		data = numpy.vstack([data,[row[0],row[7]]])
 
+print data
+print "Final row: ", data[data.shape[0]-1,]
 
 ## Thoughts:
-# What's the best way to minimally/easily show students how to make a data
-# structure?  Arrays and dicts don't seem ideal, AL suggests numpy.
+#  Is this the best way to build the data structure?  Is this the best way
+#  to show how to build this data structure?  Is that final print shape too
+#  complicated?
+
+
+# Introduces/exemplifies
+#    #  Importing from libraries (csv, numpy)
+#    #  Initializing a variable before adding to it?  (Instructors can show
+#    #  what happens if you comment that out.
+#    #  Vstack...
+#    #  Indexing into arrays.
+#    #  Finding the final element of a matrix/array/list by getting size - 1.
+
